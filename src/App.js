11d950d10590dddb7720a53029c994c97873e12a
Eduardo Garre <eduardo.garre@gmail.com>
2021-10-12 14:37:27 +0200

    Primera parte del tutorial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Cabecera from "./components/Cabecera";
+
+function App() {
+  return (
+    <div className="contenedor">
+      <Cabecera />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cabecera.js
@@ -0,0 +1,9 @@
+const Cabecera = () => {
+    return (
+        <header>
+            <h1>Lenguaje de programación Ñ</h1>
+        </header>
+    )
+}
+
+export default Cabecera
