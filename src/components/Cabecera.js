fa3faab3289d693e54143ee63d65209cbed3554f
eduardogarre <eduardo.garre@gmail.com>
2022-04-10 13:51:24 +0200

    Implemento menú desplegable

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import Boton from './Boton'
 import logo from '../img/logo21.png'
 
@@ -7,32 +8,41 @@ const Cabecera = ({titulo}) => {
         console.log(e)
     }
 
-    return (
-        <nav className="cabecera cristal navbar fixed-top navbar-expand-md navbar-nav navbar-light border-bottom">
-            <div className="container-fluid">
-                <a className="marca navbar-brand" href="#" style={{fontWeight: 300}}>
-                    &nbsp;Lenguaje&nbsp;
-                    <img src={logo} alt="" style={{width:"2.7rem", height:"2.7rem"}} className="d-inline-block align-text-bottom" />
-                </a>
+    const [menuColapsado, setMenuColapsado] = useState(true)
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#barraNavEnlaces" aria-controls="barraNavEnlaces" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <ul id="barraNavEnlaces" className="navbar-nav mr-auto collapse navbar-collapse justify-content-end">
+    const accionaNav = () => {
+        console.log("antes: " + menuColapsado)
+        setMenuColapsado(!menuColapsado)
+        console.log("después: " + menuColapsado)
+        console.log(" ")
+    }
+
+    return (
+        <nav id="cabecera" className="cristal navbar navbar-expand-md text-dark fixed-top navbar-light border-bottom">
+            <a className="marca navbar-brand" href="#" style={{fontWeight: 300}}>
+                &nbsp;Lenguaje&nbsp;
+                <img src={logo} alt="" style={{width:"2.7rem", height:"2.7rem"}} className="d-inline-block align-text-bottom" />
+            </a>
+
+            <button className="navbar-toggler" onClick={accionaNav} type="button" data-toggle="collapse" data-target="#barraNavEnlaces" aria-controls="barraNavEnlaces" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <ul id="barraNavEnlaces" className={"mr-auto collapse navbar-collapse justify-content-end" + (menuColapsado ? " collapse" : " show")}>
+                <div className="navbar-nav">
                     <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="#">Descarga</a>
+                        <a className="nav-link" aria-current="page" href="#">Descarga</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Aprende</a>
+                        <a className="nav-link" href="#">Aprende</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Documentación</a>
+                        <a className="nav-link" href="#">Documentación</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Proyecto</a>
+                        <a className="nav-link" href="#">Proyecto</a>
                     </li>
-                </ul>
-            </div>
+                </div>
+            </ul>
         </nav>
     )
 }
