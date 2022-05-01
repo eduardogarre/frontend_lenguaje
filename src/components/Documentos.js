76be6763272b88c17cbd1b0f0defbc3b7e99d4de
eduardogarre <eduardo.garre@gmail.com>
2022-05-01 16:34:42 +0200

    Instalando TinyMCE

--- src/components/Documentos.js
+++ src/components/Documentos.js
@@ -1,10 +1,35 @@
-import MenuLateral from "./MenuLateral"
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 const Documentos = () => {
+    const editorRef = useRef(null);
+    const log = () => {
+      if (editorRef.current) {
+        console.log(editorRef.current.getContent());
+      }
+    };
     return (
-        <div className="container mw-100 p-3" style={{width: "90rem", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center", alignItems: "center", columnGap: "10rem", rowGap: "5rem"}}>
-        <MenuLateral />
-        </div>
+      <>
+        <Editor
+          onInit={(evt, editor) => editorRef.current = editor}
+          initialValue="<p>This is the initial content of the editor.</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
+            ],
+            toolbar: 'undo redo | blocks | ' +
+              'bold italic forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <button onClick={log}>Log editor content</button>
+      </>
     )
 }
 
