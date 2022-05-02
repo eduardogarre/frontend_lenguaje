e7164eaf2d459e5aca370a702536bff7dbced098
eduardogarre <eduardo.garre@gmail.com>
2022-05-02 13:29:30 +0200

    Sustituye enlaces normales por componentes Link

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../img/logo21.png'
 
 const Cabecera = () => {
@@ -16,10 +17,10 @@ const Cabecera = () => {
     return (
         <nav id="cabecera" className="cristal navbar fixed-top navbar-expand-md navbar-nav navbar-light border-bottom m-0 p-0">
             <div className="container-fluid m-2 px-2">
-                <a className="marca navbar-brand m-0 mt-1 p-0" href="/" style={{ fontWeight: 400 }}>
+                <Link className="marca navbar-brand m-0 mt-1 p-0" to="/" style={{ fontWeight: 400 }}>
                     &nbsp;Lenguaje&nbsp;
                     <img src={logo} alt="" style={{ width: "2.7rem", height: "2.7rem" }} className="d-inline-block align-text-bottom" />
-                </a>
+                </Link>
 
                 <button className="navbar-toggler me-4" onClick={accionaNav} type="button" data-toggle="collapse" data-target="#barraNavEnlaces" aria-controls="barraNavEnlaces" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -27,16 +28,16 @@ const Cabecera = () => {
                 <ul id="barraNavEnlaces" className={"mr-auto collapse navbar-collapse justify-content-end m-0 me-2" + (menuColapsado ? " collapse" : " show")}>
                     <div className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Descarga</a>
+                            <Link className="nav-link" aria-current="page" to="#">Descarga</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/aprende">Aprende</a>
+                            <Link className="nav-link" to="/aprende">Aprende</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/documentacion">Documentación</a>
+                            <Link className="nav-link" to="/documentacion">Documentación</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="https://github.com/eduardogarre/lenguaje">Proyecto</a>
+                            <Link className="nav-link" to="https://github.com/eduardogarre/lenguaje">Proyecto</Link>
                         </li>
                     </div>
                 </ul>
