24a5835c025695b7a5c16e6d421ac26e091003c2
eduardogarre <eduardo.garre@gmail.com>
2022-05-08 21:33:21 +0200

    Sombreado para recuadros de código

--- src/components/Código.js
+++ src/components/Código.js
@@ -2,7 +2,7 @@ const Código = ({ código }) => {
 
     return (
         <div>
-            <pre style={{border: "0px"}}>
+            <pre style={{border: "0px", boxShadow: "0px 0px #fff"}}>
                 <code className="text-start h-100" style={{fontFamily: "Roboto Mono", textShadow: "4px 4px 8px #ddd"}}>
                     {código}
                 </code>
