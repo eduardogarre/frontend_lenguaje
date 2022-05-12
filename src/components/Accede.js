5c696b075ce99294fc5044084761e7118f579bde
eduardogarre <eduardo.garre@gmail.com>
2022-05-13 01:33:07 +0200

    Centro el componente

--- src/components/Accede.js
+++ src/components/Accede.js
@@ -28,40 +28,42 @@ function Accede() {
     }
 
     return (
-        <div className='shadow rounded-3 p-4' style={{ width: "31rem", outline: "1px solid #ddd" }}>
-            <form onSubmit={manejaEnvío}>
-                <div className="input-group mb-3">
-                    <span id="etiqueta-usuario" className='input-group-text my-0 py-0' style={{ width: "5rem" }}>Usuario</span>
-                    <input
-                        type="text"
-                        id="usuario"
-                        className='form-control my-0 py-0'
-                        ref={refUsuario}
-                        autoComplete="off"
-                        onChange={(e) => setUsuario(e.target.value)}
-                        value={usuario}
-                        aria-label="Usuario"
-                        aria-describedby="etiqueta-usuario"
-                        required
-                    />
-                </div>
-                <div className="input-group mb-3">
-                    <span id="etiqueta-clave" className='input-group-text my-0 py-0' style={{ width: "5rem" }}>Clave</span>
-                    <input
-                        type="password"
-                        id="clave"
-                        className='form-control my-0 py-0'
-                        onChange={(e) => setClave(e.target.value)}
-                        value={clave}
-                        required
-                    />
-                </div>
-                <div className='input-group pe-1' style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap" }}>
-                    <div ref={refError} className={"rounded-3 " + (éxito ? "msjerror" : "oculto")} style={{ width: "20rem", height: "3rem" }} aria-live="assertive">{msjError}</div>
-                    <div></div>
-                    <Boton texto="Entra" />
-                </div>
-            </form>
+        <div className="container mw-100 p-3" style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center", alignItems: "center" }}>
+            <div className='shadow rounded-3 p-4' style={{ width: "31rem", outline: "1px solid #ddd" }}>
+                <form onSubmit={manejaEnvío}>
+                    <div className="input-group mb-3">
+                        <span id="etiqueta-usuario" className='input-group-text my-0 py-0' style={{ width: "5rem" }}>Usuario</span>
+                        <input
+                            type="text"
+                            id="usuario"
+                            className='form-control my-0 py-0'
+                            ref={refUsuario}
+                            autoComplete="off"
+                            onChange={(e) => setUsuario(e.target.value)}
+                            value={usuario}
+                            aria-label="Usuario"
+                            aria-describedby="etiqueta-usuario"
+                            required
+                        />
+                    </div>
+                    <div className="input-group mb-3">
+                        <span id="etiqueta-clave" className='input-group-text my-0 py-0' style={{ width: "5rem" }}>Clave</span>
+                        <input
+                            type="password"
+                            id="clave"
+                            className='form-control my-0 py-0'
+                            onChange={(e) => setClave(e.target.value)}
+                            value={clave}
+                            required
+                        />
+                    </div>
+                    <div className='input-group pe-1' style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", flexWrap: "nowrap" }}>
+                        <div ref={refError} className={"rounded-3 " + (éxito ? "msjerror" : "oculto")} style={{ width: "20rem", height: "3rem" }} aria-live="assertive">{msjError}</div>
+                        <div></div>
+                        <Boton texto="Entra" />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
