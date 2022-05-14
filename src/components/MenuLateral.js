c29d4a3568f14cbc5fb60294875b8be98de4877d
eduardogarre <eduardo.garre@gmail.com>
2022-05-15 01:02:12 +0200

    Obteniendo Documentos de primer Nivel de la API

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import Acordeon from "./Acordeon"
+import { servidor } from "../Configuración";
 
 const jerarquía = [
     {
@@ -42,13 +44,58 @@ const jerarquía = [
     }
 ]
 
+async function obténDocumento(id) {
+    return fetch(servidor + '/api/v1/documento/' + id)
+        .then(respuesta => respuesta.json())
+        .then(documento => {
+            console.log(documento);
+            return documento;
+        })
+}
+
 const MenuLateral = () => {
 
-    return (
-        <div className="border rounded rounded-3 py-2 px-4" style={{ minWidth: "15rem", background: "#fafafa" }}>
-            <Acordeon inicial={true} jerarquía={jerarquía} />
-        </div>
-    )
+    const [estáCargando, setEstaCargando] = useState(true);
+    const [documentos, setDocumentos] = useState([]);
+
+    useEffect(() => {
+        if (estáCargando) {
+            fetch(servidor + '/api/v1/documento/0')
+                .then(respuesta => respuesta.json())
+                .then(async (documento) => {
+                    console.log("documento raíz")
+                    console.log(documento)
+                    documento.hijos.forEach(async (id) => {
+                        console.log("id de hijo: " + id)
+                        let hijo = await obténDocumento(id);
+                        setDocumentos(documentos.push(hijo));
+                    });
+                    return await documentos;
+                })
+                .then(async () => {
+                    console.log("Lista de todos los documentos incluidos")
+                    console.log(documentos)
+                });
+            setEstaCargando(false);
+        }
+    }, [estáCargando, documentos]);
+
+    if (estáCargando) { // Si está cargando, mostramos un texto que lo indique
+        return (
+            <center className="w-100">
+                <div>
+                    <h1><i className="bi bi-clock-history"></i></h1>
+                </div>
+            </center>
+        );
+    }
+    else {
+        return (
+            <div className="border rounded rounded-3 py-2 px-4" style={{ minWidth: "15rem", background: "#fafafa" }}>
+                <Acordeon inicial={true} jerarquía={jerarquía} />
+            </div>
+        )
+    }
 }
 
 export default MenuLateral
