98304b5d1dbc2c74730a91d0a2205cc4ef70aecb
eduardogarre <eduardo.garre@gmail.com>
2022-05-15 03:55:23 +0200

    Consulto Backend para rellenar menú lateral

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -1,52 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+import { servidor } from "../Configuración";
 import Acordeon from "./Acordeon"
-
-const jerarquía = [
-    {
-        id: 0,
-        título: "Inicio",
-        hijos: [
-
-            {
-                id: 1,
-                título: "Introducción",
-                hijos: []
-            },
-            {
-                id: 2,
-                título: "Instalación",
-                hijos: [
-                    {
-                        id: 3,
-                        título: "Windows",
-                        hijos: []
-                    },
-                    {
-                        id: 4,
-                        título: "Linux",
-                        hijos: []
-                    },
-                    {
-                        id: 5,
-                        título: "macOS",
-                        hijos: []
-                    }
-                ]
-            }
-
-        ]
-    },
-    {
-        id: 6,
-        título: "Hola, mundo",
-        hijos: []
-    }
-]
 
 const MenuLateral = () => {
 
+    const [cargando, setCargando] = useState(true);
+    const [documentos, setDocumentos] = useState([]);
+
+    const cargaDocumentos = useCallback(() => {
+        async function asincrona() {
+            let respuesta = await fetch(servidor + "/api/v1/documento/0");
+            let json = await respuesta.json();
+            let hijos = json.hijos;
+            console.log("--- HIJOS ---");
+            console.log(hijos);
+            let docus = []
+            hijos.forEach(idHijo => {
+                fetch(servidor + "/api/v1/documento/" + idHijo)
+                    .then(respuesta => respuesta.json())
+                    .then(hijo => docus.push(hijo));
+            });
+            setDocumentos(docus);
+        }
+        asincrona();
+    }, [documentos, setDocumentos]);
+
+    useEffect(() => {
+        if (cargando) {
+            cargaDocumentos();
+            setCargando(false);
+        }
+    }, [cargaDocumentos, cargando, setCargando]);
+
+    console.log("--- DOCUMENTOS ---");
+    console.log(documentos);
+
     return (
         <div className="border rounded rounded-3 py-2 px-4" style={{ minWidth: "15rem", background: "#fafafa" }}>
-            <Acordeon inicial={true} jerarquía={jerarquía} />
+            <Acordeon documentos={documentos} />
         </div>
     )
 }
