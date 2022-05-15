1a557607451379c8a6a4dfa0fa7dd2c5b76d3bc9
eduardogarre <eduardo.garre@gmail.com>
2022-05-16 01:51:23 +0200

    Acordeón consigue la lista de documentos

--- src/components/Documentación/MenúLateral/Acordeon.js
+++ src/components/Documentación/MenúLateral/Acordeon.js
@@ -1,10 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
+import { servidor } from "../../../Configuración";
 import { Link } from 'react-router-dom'
 import AcordeonBoton from "./AcordeonBoton"
 import AcordeonContenido from "./AcordeonContenido"
 import _uniqueId from 'lodash/uniqueId';
 
-const Acordeon = ({ idRaíz, documentos }) => {
+const Acordeon = ({ idRaíz }) => {
+
+    const [cargando, setCargando] = useState(true);
+    const [documentos, setDocumentos] = useState([]);
+
+    const cargaDocumentos = useCallback(async () => {
+        async function asincrona() {
+            let respuesta = await fetch(servidor + "/api/v1/documento/0");
+            let json = await respuesta.json();
+            let hijos = json.hijos;
+            let docus = [];
+            hijos.forEach((idHijo) => {
+                fetch(servidor + "/api/v1/documento/" + idHijo)
+                    .then(respuesta => respuesta.json())
+                    .then(hijo => {
+                        docus = [...docus, hijo]
+                        setDocumentos(docus)
+                    })
+            });
+        }
+        await asincrona();
+    }, [setDocumentos]);
+
+    useEffect(() => {
+        if (cargando) {
+            cargaDocumentos();
+            setCargando(false);
+        }
+    }, [cargaDocumentos, cargando]);
 
     const [haRenderizado, renderiza] = useState(false);
     const [idAcordeon] = useState(_uniqueId('idAcordeon-'));
@@ -23,30 +52,40 @@ const Acordeon = ({ idRaíz, documentos }) => {
 
     return (
         <div id={idAcordeon} className="accordion accordion-flush bg-transparent w-100 m-0 p-0">
-            
-            {
-                documentos.map((elemento) => {
-                    console.log("Acordeon: documentos.map() ... elemento N \n");
-                    console.log(elemento);
 
-                    return (
-                        <div className="accordion-item bg-transparent m-0 p-0">
 
-                            {(elemento.hijos.length > 0) && (
-                                <>
-                                    <AcordeonBoton idPadre={idAcordeon} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} título={elemento.id + " " + elemento.título} />
-                                    <AcordeonContenido idPadre={idAcordeon} idArtículo={elemento.id} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} contenido={elemento.hijos} />
-                                </>
-                            )}
+            {(cargando || documentos.length === 0) && (
+                <center className="p-3"><h1><i className="bi bi-clock-history"></i></h1></center>
+            )}
 
-                            {(elemento.hijos.length === 0) && (
-                                <AcordeonContenido idPadre={idAcordeon} idArtículo={elemento.id} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} contenido={elemento.id + " " + elemento.título} />
-                            )}
+            {(!cargando && documentos.length > 0) && (
+                <>
+                    {
+                        documentos.map((elemento) => {
+                            console.log("Acordeon: documentos.map() ... elemento N \n");
+                            console.log(elemento);
 
-                        </div>
-                    )
-                })
-            }
+                            return (
+                                <div className="accordion-item bg-transparent m-0 p-0">
+
+                                    {(elemento.hijos.length > 0) && (
+                                        <>
+                                            <AcordeonBoton idPadre={idAcordeon} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} título={elemento.id + " " + elemento.título} />
+                                            <AcordeonContenido idPadre={idAcordeon} idArtículo={elemento.id} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} contenido={elemento.hijos} />
+                                        </>
+                                    )}
+
+                                    {(elemento.hijos.length === 0) && (
+                                        <AcordeonContenido idPadre={idAcordeon} idArtículo={elemento.id} idContenido={idAcordeonContenido + "-" + elemento.id} idBotón={idAcordeonBotón + "-" + elemento.id} contenido={elemento.id + " " + elemento.título} />
+                                    )}
+
+                                </div>
+                            )
+                        })
+                    }
+                </>
+
+            )}
             <div className='mt-1 m-0 p-0'>
                 <Link className='enlace d-flex flex-row align-items-center m-0 p-0' to={"/edita/" + idRaíz + "/crea"}>
                     <i className="m-0 p-0 bi bi-plus fs-3"></i>
--- src/components/Documentación/MenúLateral/MenúLateral.js
+++ src/components/Documentación/MenúLateral/MenúLateral.js
@@ -1,46 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
-import { servidor } from "../../../Configuración";
 import Acordeon from "./Acordeon"
 
 const MenúLateral = () => {
 
-    const [cargando, setCargando] = useState(true);
-    const [documentos, setDocumentos] = useState([]);
-
-    const cargaDocumentos = useCallback(async () => {
-        async function asincrona() {
-            let respuesta = await fetch(servidor + "/api/v1/documento/0");
-            let json = await respuesta.json();
-            let hijos = json.hijos;
-            let docus = [];
-            hijos.forEach((idHijo) => {
-                fetch(servidor + "/api/v1/documento/" + idHijo)
-                    .then(respuesta => respuesta.json())
-                    .then(hijo => {
-                        docus = [...docus, hijo]
-                        setDocumentos(docus)
-                    })
-            });
-        }
-        await asincrona();
-    }, [setDocumentos]);
-
-    useEffect(() => {
-        if (cargando) {
-            cargaDocumentos();
-            setCargando(false);
-        }
-    }, [cargaDocumentos, cargando]);
-
     return (
         <div className="border rounded rounded-3 py-2 px-4" style={{ minWidth: "15rem", background: "#fafafa" }}>
-            {(cargando || documentos.length === 0) && (
-                <center className="p-3"><h1><i className="bi bi-clock-history"></i></h1></center>
-            )}
-
-            {(!cargando && documentos.length > 0) && (
-                <Acordeon idRaíz={0} documentos={documentos} />
-            )}
+            <Acordeon idRaíz={0}/>
         </div>
     )
 }
