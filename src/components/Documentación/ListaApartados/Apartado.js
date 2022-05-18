f14b49ec01925c5038718a37b96184334842ec31
eduardogarre <eduardo.garre@gmail.com>
2022-05-18 11:34:43 +0200

    Arreglo deslizamiento hasta elemento

--- src/components/Documentación/ListaApartados/Apartado.js
+++ src/components/Documentación/ListaApartados/Apartado.js
@@ -1,8 +1,20 @@
 
-const Apartado = ({documento}) => {
+const Apartado = ({ documento }) => {
     console.log("LISTA APARTADOS")
+
+    const deslízateHastaElemento = () => {
+        const id = documento.referencia;
+        const margenSuperior = -75;
+        const elemento = document.getElementById(id);
+        const y = elemento.getBoundingClientRect().top + window.pageYOffset + margenSuperior;
+
+        window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+
     return (
-        <div>{documento.referencia + " " + documento.título}</div>
+        <div>
+            <a onClick={deslízateHastaElemento}>{documento.referencia + " " + documento.título}</a>
+        </div>
     )
 }
 
