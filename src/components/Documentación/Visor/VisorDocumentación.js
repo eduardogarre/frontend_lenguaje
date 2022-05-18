4e3bd1b076158cf21108dc657f33614067f72ada
eduardogarre <eduardo.garre@gmail.com>
2022-05-18 10:33:05 +0200

    Borro código inservible

--- src/components/Documentación/Visor/VisorDocumentación.js
+++ src/components/Documentación/Visor/VisorDocumentación.js
@@ -7,8 +7,6 @@ const VisorDocumentación = ({ cargando }) => {
 
     const [estáCargando, setEstáCargando] = useState(cargando);
     const [árbol, setÁrbol] = useState(null);
-    //const [documento, setDocumento] = useState(null);
-    //const [documentos, setDocumentos] = useState([]);
 
     let params = useParams();
 
@@ -97,109 +95,6 @@ const VisorDocumentación = ({ cargando }) => {
             }
         }
     });
-
-    /*
-    const cargaDocumentos = useCallback(async () => {
-        async function asincrona() {
-            let hijos = documento.hijos;
-            let docus = [];
-            hijos.forEach((idHijo) => {
-                fetch(servidor + "/api/v1/documento/" + idHijo)
-                    .then(respuesta => respuesta.json())
-                    .then(hijo => {
-                        docus = [...docus, hijo]
-
-                        console.log("Consigo hijo con id: " + hijo.id);
-
-                        // Establezco los documentos, en el orden en el que aparecen
-                        // en el array documento.hijos del padre
-                        if (docus.length === hijos.length) {
-                            let docs = hijos.map((idc) => {
-                                let doc = docus.findIndex((d) => idc === d.id);
-                                return docus[doc];
-                            });
-                            setDocumentos(docs);
-                            console.log("Documentos hijos conseguidos");
-                            console.log(docs);
-                            setEstaCargando(false);
-
-                            console.log("ÁRBOL DE DOCUMENTOS");
-                            console.log(cargaÁrbolDocumentos(documento.id));
-                        }
-                    })
-            });
-        }
-
-        await asincrona();
-    }, [id, documento, estáCargando, setDocumentos, setEstaCargando]);
-
-    useEffect(() => {
-
-        let pideDocumento = false;
-        let pideDocumento1 = false;
-        let pideDocumento2 = false;
-        let pideDocumento3 = false;
-
-        if (documento && documento.id != id) {
-            pideDocumento1 = true;
-        }
-        if (documento && documento.hijos.length != documentos.length) {
-            console.log("Los hijos no concuerdan");
-            if (documento.hijos.length <= 0) {
-                console.log("Realmente el documento actual no tiene hijos, los borro");
-                setDocumentos([]);
-                pideDocumento2 = false;
-            }
-            else {
-                console.log("Los hijos del documento actual no han cargado, los pido");
-                cargaDocumentos();
-            }
-        }
-        if (estáCargando && id > -1) {
-            pideDocumento3 = true;
-        }
-
-        pideDocumento = pideDocumento1 || pideDocumento2 || pideDocumento3;
-
-        //console.log("DEPURACIÓN");
-        //console.log("documento");
-        //console.log(documento);
-        //if (documento) {
-        //    console.log("documento.id");
-        //    console.log(documento.id);
-        //    console.log("documentos.length");
-        //    console.log(documentos.length);
-        //    console.log("documento.hijos.length");
-        //    console.log(documento.hijos.length);
-        //}
-        //console.log("documentos");
-        //console.log(documentos);
-        //console.log("id");
-        //console.log(id);
-        //console.log("estáCargando");
-        //console.log(estáCargando);
-        //console.log("pideDocumento");
-        //console.log(pideDocumento);
-        //console.log("pideDocumento1");
-        //console.log(pideDocumento1);
-        //console.log("pideDocumento2");
-        //console.log(pideDocumento2);
-        //console.log("pideDocumento3");
-        //console.log(pideDocumento3);
-        
-        if (pideDocumento) {
-            fetch(servidor + '/api/v1/documento/' + id)
-                .then(respuesta => respuesta.json())
-                .then(async (dato) => {
-                    setDocumento(dato);
-                    console.log("Consigo documento con id: " + dato.id);
-                    if (dato && dato.hijos.length === 0) {
-                        setEstaCargando(false);
-                    }
-                })
-        }
-    }, [id, documento, documentos, estáCargando, cargaDocumentos]);
-    */
 
     if (estáCargando) { // Si está cargando, mostramos un texto que lo indique
         return (
