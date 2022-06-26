5795971da6d9a5acf15a0bdf6ea0a19b29191c72
eduardogarre <eduardo.garre@gmail.com>
2022-06-26 09:48:04 +0200

    Errata: Mismo intervalo elementos carrusel

--- src/components/Portada/Carrusel/Carrusel.js
+++ src/components/Portada/Carrusel/Carrusel.js
@@ -2,15 +2,17 @@ import Código from '../../Código';
 
 const Carrusel = () => {
 
-    return (
-        <div id="carruselCódigo" className="carousel slide carousel-fade carousel-dark shadow rounded-3 fs-6" data-bs-ride="carousel" style={{height: "22rem", width: "31rem", outline: "1px solid #01756f"}}>
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carruselCódigo" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carruselCódigo" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            </div>
-            <div className="carousel-inner p-3 pt-2">
-                <div className="carousel-item active" data-bs-interval="10000">
-                    <Código código={`// ¡Hola, Mundo!
+  const intervalo = 5000
+
+  return (
+    <div id="carruselCódigo" className="carousel slide carousel-fade carousel-dark shadow rounded-3 fs-6" data-bs-ride="carousel" style={{ height: "22rem", width: "31rem", outline: "1px solid #01756f" }}>
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carruselCódigo" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carruselCódigo" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      </div>
+      <div className="carousel-inner p-3 pt-2">
+        <div className="carousel-item active" data-bs-interval={intervalo}>
+          <Código código={`// ¡Hola, Mundo!
 
 // Función de la biblioteca estándar
 externo ent escribe(nat8* texto);
@@ -22,9 +24,9 @@ público ent inicia()
 
   devuelve 0;
 }`} />
-                </div>
-                <div className="carousel-item" data-bs-interval="5000">
-                    <Código código={`// Factorial de un número natural
+        </div>
+        <div className="carousel-item" data-bs-interval={intervalo}>
+          <Código código={`// Factorial de un número natural
 nat factorial(nat número)
 {
   si (número == 1)
@@ -36,10 +38,10 @@ nat factorial(nat número)
     devuelve número * factorial(número - 1);
   }
 }`} />
-                </div>
-            </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Carrusel
